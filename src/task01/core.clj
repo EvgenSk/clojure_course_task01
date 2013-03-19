eb336494cdd26dfd1dfe99d1b8ab5ed087e894c8
EvgenSk <evgensk@gmail.com>
2013-03-19 21:36:23 +0700

    code-review changes. code is clearer now.

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -2,16 +2,17 @@
   (:require [pl.danieljanus.tagsoup :refer :all])
   (:gen-class))
 
-(defn get-links-1 [container]
-  (cond (and (vector? container) (= (first container) :h3))
-        (get (get (get container 2) 1) :href)
-        (vector? container)
-        (flatten (filter #(not (or (nil? %) (empty? %))) (map #(get-links-1 %) container)))
-        :else nil))
+(defn get-links 
+  ([] (let [data (parse "clojure_google.html")]
+        (get-links data)))
 
-(defn get-links []
-  (let [data (parse "clojure_google.html")]
-    (get-links-1 data)))
+  ([container] (cond (and (vector? container) (= (first container) :h3))
+                     (get-in container [2 1 :href])
+                     (vector? container)
+                     (->> (map get-links container)
+                          (remove empty?)
+                          (flatten))
+                     :else nil)))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
